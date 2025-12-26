1700c9b392d5c649658f10354f5b1919e0b73e42
marwan.salman <markamel@publicisgroupe.net>
2025-12-26 19:34:58 +0100

    add swager docs

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './common/guards/auth/auth.guard';
 import { RolesGuard } from './common/guards/roles/roles.guard';
 import { GlobalGuard } from './common/guards/globalGuard';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {  
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,17 @@ async function bootstrap() {
     credentials: true,               // if you need cookies or auth headers
   });
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Tournament API')
+    .setDescription('Complete tournament management system')
+    .setVersion('1.0')
+    .addBearerAuth()  // ✅ JWT support
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.APPLICATION_PORT ?? 3000);
 }
 bootstrap();
