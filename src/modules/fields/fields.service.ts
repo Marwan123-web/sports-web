a88262edc0756221cd4da39df2dc5e9d41e2816c
marwan.salman <markamel@publicisgroupe.net>
2026-01-20 01:12:48 +0100

    fix fields issue

--- src/modules/fields/fields.service.ts
+++ src/modules/fields/fields.service.ts
@@ -31,17 +31,19 @@ export class FieldsService {
       .leftJoinAndSelect('field.bookings', 'booking')
       .leftJoinAndSelect('booking.user', 'user')
       .where('field.isActive = :isActive', { isActive: true });
-  
+
     if (filters?.sport) {
-      query.andWhere('field.sport::text ILike :sport', { sport: `%${filters.sport}%` });
-    }
-  
-    if (filters?.date) {
-      query.andWhere('(booking.date = :date OR booking.date IS NULL)', {
-        date: filters.date,
+      query.andWhere('field.sport::text ILike :sport', {
+        sport: `%${filters.sport}%`,
       });
     }
-  
+
+    // if (filters?.date) {
+    //   query.andWhere('(booking.date = :date OR booking.date IS NULL)', {
+    //     date: filters.date,
+    //   });
+    // }
+
     if (filters?.q) {
       const like = `%${filters.q}%`;
       query.andWhere(
@@ -49,18 +51,24 @@ export class FieldsService {
         { like },
       );
     }
-  
+
     if (filters?.priceFrom !== undefined) {
-      query.andWhere('field.pricePerHour >= :priceFrom', { priceFrom: filters.priceFrom });
+      query.andWhere('field.pricePerHour >= :priceFrom', {
+        priceFrom: filters.priceFrom,
+      });
     }
     if (filters?.priceTo !== undefined) {
-      query.andWhere('field.pricePerHour <= :priceTo', { priceTo: filters.priceTo });
+      query.andWhere('field.pricePerHour <= :priceTo', {
+        priceTo: filters.priceTo,
+      });
     }
-  
+
     if (filters?.address) {
-      query.andWhere('field.address ILike :city', { city: `%${filters.address}%` });
+      query.andWhere('field.address ILike :city', {
+        city: `%${filters.address}%`,
+      });
     }
-  
+
     return query.orderBy('field.pricePerHour', 'ASC').getMany();
   }
 
